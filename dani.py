77105a7d2560d3c4dbfccabd9d6ae84fe98275f2
danish123gupta <40190986+danish123gupta@users.noreply.github.com>
2018-09-30 00:51:21 +0530

    Add files via upload

---
+++ dani.py
@@ -0,0 +1,66 @@
+#q.no.1
+x=list()
+print(x)
+
+#q.no.2
+x=['google','apple','facebook','microsoft','tesla']
+print(x)
+
+#q.no.3
+print(x.count("google"))
+print(x.count("apple"))
+print(x.count("microsoft"))
+print(x.count("facebook"))
+print(x.count("tesla"))
+
+#q.no.4
+y=[4,2,7,9,5,3,1]
+y.sort()
+print(y)
+
+#q.no.5
+a=[12,76,89,42]
+b=[4,5,6,7]
+c=a+b
+c.sort()
+print(c)
+
+#q.no.6
+numbers=[9,8,7,6,5,7]
+count_odd=0
+count_even=0
+for x in numbers:
+    if x%2==0:
+        count_even+=1
+print("no of odd number",count_odd)
+print("no of even numbers",count_even)
+
+#q.no.7
+t=("danish","ramesh","suresh","nimesh")
+print(list(reversed(t)))
+
+#q.no.8
+a=(100,66,77,90,88,22,11)
+print(max(a))
+print(min(a))
+
+
+
+#q.no.9
+value="danish"
+x=value.upper()
+print(x)
+
+
+#q.no.10
+a="4567"
+print(a.isdigit())
+
+#q.no.11
+a="hello world"
+print(a.replace("world","danish"))
+
+
+
+
+#
